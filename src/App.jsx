b746c1341e8a2132522d959541ef3ec96dcd4847
richietaylor <richard@taylorfamily.co.za>
2024-05-30 15:57:47 +0200

    Set up for ghp

--- src/App.jsx
+++ src/App.jsx
@@ -10,12 +10,40 @@ import ReactFlow, {
  
 import 'reactflow/dist/style.css';
  
+// const initialNodes = [
+//   { id: '1', position: { x: 0, y: 0 }, data: { label: '1' } },
+//   { id: '2', position: { x: 0, y: 100 }, data: { label: '2' } },
+// ];
+// const initialEdges = [{ id: 'e1-2', source: '1', target: '2' }];
+
 const initialNodes = [
-  { id: '1', position: { x: 0, y: 0 }, data: { label: '1' } },
-  { id: '2', position: { x: 0, y: 100 }, data: { label: '2' } },
+  {
+    id: '1',
+    type: 'input',
+    data: { label: 'Input Node' },
+    position: { x: 250, y: 25 },
+  },
+
+  {
+    id: '2',
+    // you can also pass a React component as a label
+    data: { label: <div>Default Node</div> },
+    position: { x: 100, y: 125 },
+  },
+  {
+    id: '3',
+    type: 'output',
+    data: { label: 'Output Node' },
+    position: { x: 250, y: 250 },
+  },
 ];
-const initialEdges = [{ id: 'e1-2', source: '1', target: '2' }];
- 
+
+const initialEdges = [
+  { id: 'e1-2', source: '1', target: '2' },
+  { id: 'e2-3', source: '2', target: '3', animated: true },
+  { id: 'e1-3', source: '1', target: '3', type: 'step' },
+];
+
 export default function App() {
   const [nodes, setNodes, onNodesChange] = useNodesState(initialNodes);
   const [edges, setEdges, onEdgesChange] = useEdgesState(initialEdges);
