a56d7c87c112159deac416788079c9e1d7af5973
richietaylor <richard@taylorfamily.co.za>
2024-06-03 17:44:55 +0200

    Upload and Download buttons not working >:(

--- src/App.jsx
+++ src/App.jsx
@@ -1,49 +1,3 @@
-// import React, { useCallback } from 'react';
-// import ReactFlow, {
-//   MiniMap,
-//   Controls,
-//   Background,
-//   useNodesState,
-//   useEdgesState,
-//   addEdge,
-// } from 'reactflow';
- 
-// import DownloadButton from './DownloadButton.jsx';
-// // import CustomNode from './CustomNode';
-// import { initialNodes, initialEdges } from './nodes-edges';
-// import ShapePalette from './ShapePalette';
-
-// import 'reactflow/dist/style.css';
- 
-
-
-// export default function App() {
-//   const [nodes, setNodes, onNodesChange] = useNodesState(initialNodes);
-//   const [edges, setEdges, onEdgesChange] = useEdgesState(initialEdges);
- 
-//   const onConnect = useCallback(
-//     (params) => setEdges((eds) => addEdge(params, eds)),
-//     [setEdges],
-//   );
- 
-//   return (
-//     <div style={{ width: '100vw', height: '100vh' }}>
-//       <ReactFlow
-//         nodes={nodes}
-//         edges={edges}
-//         onNodesChange={onNodesChange}
-//         onEdgesChange={onEdgesChange}
-//         onConnect={onConnect}
-//       >
-//         <Controls />
-//         <MiniMap />
-//         <Background variant="dots" gap={12} size={1} />
-//         <DownloadButton />
-//       </ReactFlow>
-//     </div>
-//   );
-// }
-
 import React, { useCallback } from 'react';
 import ReactFlow, {
   MiniMap,
@@ -53,70 +7,125 @@ import ReactFlow, {
   useEdgesState,
   addEdge,
 } from 'reactflow';
-
-import ShapePalette from './ShapePalette'; // Import the ShapePalette component
+ 
 import DownloadButton from './DownloadButton.jsx';
 // import CustomNode from './CustomNode';
+import SaveButton from './SaveButton.jsx';
+import UploadButton from './UploadButton.jsx';
 import { initialNodes, initialEdges } from './nodes-edges';
+// import ShapePalette from './ShapePalette';
 
 import 'reactflow/dist/style.css';
+ 
+
 
 export default function App() {
   const [nodes, setNodes, onNodesChange] = useNodesState(initialNodes);
   const [edges, setEdges, onEdgesChange] = useEdgesState(initialEdges);
-
+ 
   const onConnect = useCallback(
     (params) => setEdges((eds) => addEdge(params, eds)),
     [setEdges],
   );
 
-  const onDrop = useCallback(
-    (event) => {
-      event.preventDefault();
-
-      const reactFlowBounds = event.target.getBoundingClientRect();
-      const type = event.dataTransfer.getData('application/reactflow');
-      const position = reactFlow.project({
-        x: event.clientX - reactFlowBounds.left,
-        y: event.clientY - reactFlowBounds.top,
-      });
-
-      const newNode = {
-        id: (nodes.length + 1).toString(),
-        type,
-        position,
-        data: { label: `${type} node` },
-      };
-
-      setNodes((nds) => nds.concat(newNode));
-    },
-    [nodes, setNodes],
-  );
-
-  const onDragOver = useCallback((event) => {
-    event.preventDefault();
-    event.dataTransfer.dropEffect = 'move';
-  }, []);
-
+  const loadGraph = (graph) => {
+    setNodes(graph.nodes || []);
+    setEdges(graph.edges || []);
+  };
+ 
   return (
-    <div style={{ display: 'flex', width: '100vw', height: '100vh' }}>
-      <ShapePalette />
-      <div className="reactflow-wrapper" style={{ flex: 1 }}>
-        <ReactFlow
-          nodes={nodes}
-          edges={edges}
-          onNodesChange={onNodesChange}
-          onEdgesChange={onEdgesChange}
-          onConnect={onConnect}
-          onDrop={onDrop}
-          onDragOver={onDragOver}
-        >
-          <Controls />
-          <MiniMap />
-          <Background variant="dots" gap={12} size={1} />
-          <DownloadButton />
-        </ReactFlow>
-      </div>
+    <div style={{ width: '100vw', height: '100vh' }}>
+      <ReactFlow
+        nodes={nodes}
+        edges={edges}
+        onNodesChange={onNodesChange}
+        onEdgesChange={onEdgesChange}
+        onConnect={onConnect}
+      >
+        <Controls />
+        <MiniMap />
+        <Background variant="dots" gap={12} size={1} />
+        <DownloadButton />
+        <SaveButton nodes={nodes} edges={edges} />
+        <UploadButton onLoad={loadGraph} />
+      </ReactFlow>
     </div>
   );
 }
+
+// import React, { useCallback } from 'react';
+// import ReactFlow, {
+//   MiniMap,
+//   Controls,
+//   Background,
+//   useNodesState,
+//   useEdgesState,
+//   addEdge,
+// } from 'reactflow';
+
+// import ShapePalette from './ShapePalette'; // Import the ShapePalette component
+// import DownloadButton from './DownloadButton.jsx';
+// // import CustomNode from './CustomNode';
+// import { initialNodes, initialEdges } from './nodes-edges';
+
+// import 'reactflow/dist/style.css';
+
+// export default function App() {
+//   const [nodes, setNodes, onNodesChange] = useNodesState(initialNodes);
+//   const [edges, setEdges, onEdgesChange] = useEdgesState(initialEdges);
+
+//   const onConnect = useCallback(
+//     (params) => setEdges((eds) => addEdge(params, eds)),
+//     [setEdges],
+//   );
+
+//   const onDrop = useCallback(
+//     (event) => {
+//       event.preventDefault();
+
+//       const reactFlowBounds = event.target.getBoundingClientRect();
+//       const type = event.dataTransfer.getData('application/reactflow');
+//       const position = reactFlow.project({
+//         x: event.clientX - reactFlowBounds.left,
+//         y: event.clientY - reactFlowBounds.top,
+//       });
+
+//       const newNode = {
+//         id: (nodes.length + 1).toString(),
+//         type,
+//         position,
+//         data: { label: `${type} node` },
+//       };
+
+//       setNodes((nds) => nds.concat(newNode));
+//     },
+//     [nodes, setNodes],
+//   );
+
+//   const onDragOver = useCallback((event) => {
+//     event.preventDefault();
+//     event.dataTransfer.dropEffect = 'move';
+//   }, []);
+
+//   return (
+//     <div style={{ display: 'flex', width: '100vw', height: '100vh' }}>
+//       <ShapePalette />
+//       <div className="reactflow-wrapper" style={{ flex: 1 }}>
+//         <ReactFlow
+//           nodes={nodes}
+//           edges={edges}
+//           onNodesChange={onNodesChange}
+//           onEdgesChange={onEdgesChange}
+//           onConnect={onConnect}
+//           onDrop={onDrop}
+//           onDragOver={onDragOver}
+//         >
+//           <Controls />
+//           <MiniMap />
+//           <Background variant="dots" gap={12} size={1} />
+//           <DownloadButton />
+//         </ReactFlow>
+//       </div>
+//     </div>
+//   );
+// }
