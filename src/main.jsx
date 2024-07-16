3e633320a82f2e8e9d4f771335c4d2e83f002d5e
richietaylor <richard@taylorfamily.co.za>
2024-07-16 15:24:20 +0200

    Drag+drop not working

---
+++ src/main.jsx
@@ -0,0 +1,12 @@
+// src/main.jsx
+import React from 'react';
+import ReactDOM from 'react-dom';
+import ERDiagram from './App';
+import './index.css';
+
+ReactDOM.render(
+  <React.StrictMode>
+    <ERDiagram />
+  </React.StrictMode>,
+  document.getElementById('root')
+);
