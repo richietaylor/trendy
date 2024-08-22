0ea3087c5d0c594c53ec3e2d6b3690fceafa2aa5
StephanMaree <stephan1maree@gmail.com>
2024-08-22 12:03:14 +0200

    Minor Trigger tweaks
    Fixed some logic issues

--- src/components/schema-generation/Trigger.js
+++ src/components/schema-generation/Trigger.js
@@ -291,7 +291,7 @@ class Trigger {
                     text += "\t\tIF done THEN\n";
                     text += "\t\t\tLEAVE read_loop;\n";
                     text += "\t\tEND IF;\n\n";
-                    text += "\t\tIF prev_end IS NULL OR PREV_END = cur_start THEN\n";
+                    text += "\t\tIF prev_end IS NULL OR prev_end = cur_start THEN\n";
                     text += "\t\t\tSET total_time = total_time + TIMESTAMPDIFF(" + this.unit + ", cur_start, cur_end);\n";
                     text += "\t\t\tSET prev_end = cur_end;\n"
                     text += "\t\tELSE\n";
@@ -316,6 +316,8 @@ class Trigger {
                     text += "\t\t\t(NEW." + this.finalEntity.name + "_end > " + this.initialEntity.name + "_start AND NEW." + this.finalEntity.name + "_start <= " + this.initialEntity.name +"_start)\n";
                     text += "\t\t\tOR\n";
                     text += "\t\t\t(NEW." + this.finalEntity.name + "_end >= " + this.initialEntity.name + "_end AND NEW." + this.finalEntity.name + "_start < " + this.initialEntity.name +"_end)\n";
+                    text += "\t\t\tOR\n";
+                    text += "\t\t\t(NEW." + this.finalEntity.name + "_start >= " + this.initialEntity.name + "_start AND NEW." + this.finalEntity.name + "_end <= " + this.initialEntity.name +"_end)\n";
                     text += "\t\t);\n";                      
                     text += "\tIF total_overlap > 0 THEN\n";
                     text += "\t\tSIGNAL SQLSTATE '45000'\n";
@@ -396,7 +398,7 @@ class Trigger {
                 break;
             case "ext":
                 if (this.duration === 0 && !this.optional) {
-                    text += "CREATE TRIGGER " + this.initialEntity.name + "_ext_to_" + this.finalEntity.name + "_1\n";
+                    text += "CREATE TRIGGER " + this.initialEntity.name + "_ext_to_" + this.finalEntity.name + "\n";
                     text += "BEFORE INSERT ON " + this.finalEntity.name + "\n";
                     text += "FOR EACH ROW\n";
                     text += "BEGIN\n";
@@ -419,14 +421,14 @@ class Trigger {
                         text += "\t\tSIGNAL SQLSTATE '45000'\n";
                         text += "\t\tSET MESSAGE_TEXT = '" + this.finalEntity.name + " must extend from " + this.initialEntity.name + ".';\n";
                         text += "\tEND IF;\n";
-                        text += "END;\n//\n\n";
+                        text += "END;\n//";
                     } else {
                         text += "\t\tAND (" + this.initialEntity.name + "_start <= NEW." + this.finalEntity.name + "_start AND " + this.initialEntity.name + "_end >= NEW." + this.finalEntity.name + "_end);\n";
                         text += "\tIF initial_exists = 0 THEN\n";
                         text += "\t\tSIGNAL SQLSTATE '45000'\n";
                         text += "\t\tSET MESSAGE_TEXT = '" + this.finalEntity.name + " must always extend from " + this.initialEntity.name + ".';\n";
                         text += "\tEND IF;\n";
-                        text += "END;\n//\n\n";
+                        text += "END;\n//";
                     }
                 } else if (this.duration === 0 && this.optional) {
                     // Do Nothing?
@@ -525,7 +527,7 @@ class Trigger {
             case "CHG":
                 if (this.duration === 0) { // Prevent overlap
                     // This works like chg, but it only enforces periods not overlapping if it came from A
-                    text += "CREATE TRIGGER " + this.initialEntity.name + "_chg_to_" + this.finalEntity.name + "_1\n";
+                    text += "CREATE TRIGGER " + this.initialEntity.name + "_CHG_to_" + this.finalEntity.name + "_1\n";
                     text += "BEFORE INSERT ON " + this.finalEntity.name + "\n";
                     text += "FOR EACH ROW\n";
                     text += "BEGIN\n";
@@ -556,7 +558,7 @@ class Trigger {
                     text += "END;\n//\n\n";
                     // cannot insert date in initial that overlaps with final
                     if (!this.pinned) {
-                        text += "CREATE TRIGGER " + this.initialEntity.name + "_chg_to_" + this.finalEntity.name + "_2\n";
+                        text += "CREATE TRIGGER " + this.initialEntity.name + "_CHG_to_" + this.finalEntity.name + "_2\n";
                         text += "BEFORE INSERT ON " + this.initialEntity.name + "\n";
                         text += "FOR EACH ROW\n";
                         text += "BEGIN\n";
@@ -587,7 +589,7 @@ class Trigger {
                         text += "END;\n//";
                     }
                     if (this.pinned) {
-                        text += "CREATE TRIGGER " + this.initialEntity.name + "_chg_to_" + this.finalEntity.name + "_2\n";
+                        text += "CREATE TRIGGER " + this.initialEntity.name + "_CHG_to_" + this.finalEntity.name + "_2\n";
                         text += "BEFORE INSERT ON " + this.initialEntity.name + "\n";
                         text += "FOR EACH ROW\n";
                         text += "BEGIN\n";
@@ -612,8 +614,84 @@ class Trigger {
                         text += "END;\n//";
                     }
                 } else if (this.duration !== 0) {
+                    text += "CREATE TRIGGER " + this.initialEntity.name + "_CHG_to_" + this.finalEntity.name + "_1\n";
+                    text += "BEFORE INSERT ON " + this.finalEntity.name + "\n";
+                    text += "FOR EACH ROW\n";
+                    text += "BEGIN\n";
+                    text += "\tDECLARE total_overlap INT DEFAULT 0;\n";
+                    text += "\tDECLARE total_time INT DEFAULT 0;\n";
+                    text += "\tDECLARE prev_end DATE DEFAULT NULL;\n";
+                    text += "\tDECLARE cur_start DATE;\n";
+                    text += "\tDECLARE cur_end DATE;\n";
+                    text += "\tDECLARE done INT DEFAULT 0;\n";
+                    text += "\tDECLARE cursor_a CURSOR FOR\n";
+                    text += "\t\tSELECT " + this.initialEntity.name + "_start, " + this.initialEntity.name + "_end\n";
+                    text += "\t\tFROM " + this.initialEntity.name + "\n";
+                    if (this.initialEntity.getPrimaryKey().length === 1) {
+                        text += "\t\tWHERE NEW." + this.finalEntity.getPrimaryKey()[0].split(" ")[0] + " = " + this.initialEntity.getPrimaryKey()[0].split(" ")[0] + "\n";
+                    } else if (this.initialEntity.getPrimaryKey().length === 2){
+                        text += "\t\tWHERE (\n";
+                        text += "\t\t\t(NEW." + this.finalEntity.getPrimaryKey()[0].split(" ")[0] + " = " + this.initialEntity.getPrimaryKey()[0].split(" ")[0] + " AND NEW." + this.finalEntity.getPrimaryKey()[1].split(" ")[0] + " = " + this.initialEntity.getPrimaryKey()[1].split(" ")[0] + ")\n";
+                        text += "\t\t\tOR\n";
+                        text += "\t\t\t(NEW." + this.finalEntity.getPrimaryKey()[0].split(" ")[0] + " = " + this.initialEntity.getPrimaryKey()[1].split(" ")[0] + " AND NEW." + this.finalEntity.getPrimaryKey()[1].split(" ")[0] + " = " + this.initialEntity.getPrimaryKey()[0].split(" ")[0] + ")\n";
+                        text += "\t\t\t)\n";
+                    }
+                    text += "\t\tAND " + this.initialEntity.name + "_end <= NEW." + this.finalEntity.name + "_start\n";
+                    text += "\t\tORDER BY " + this.initialEntity.name + "_start DESC;\n";
+                    text += "\tDECLARE CONTINUE HANDLER FOR NOT FOUND SET done = 1;\n\n";
+                    text += "\tOPEN cursor_a;\n\n";
+                    text += "\tread_loop: LOOP\n";
+                    text += "\t\tFETCH cursor_a INTO cur_start, cur_end;\n";
+                    text += "\t\tIF done THEN\n";
+                    text += "\t\t\tLEAVE read_loop;\n";
+                    text += "\t\tEND IF;\n\n";
+                    text += "\t\tIF cur_end = NEW." + this.finalEntity.name + "_start OR prev_end = cur_end THEN\n";
+                    text += "\t\t\tSET total_time = total_time + TIMESTAMPDIFF(" + this.unit + ", cur_start, cur_end);\n";
+                    text += "\t\t\tSET prev_end = cur_start;\n"
+                    text += "\t\tELSE\n";
+                    text += "\t\t\tLEAVE read_loop;\n";
+                    text += "\t\tEND IF;\n";
+                    text += "\tEND LOOP;\n\n";
+                    text += "\tCLOSE cursor_a;\n\n";
+                    text += "\tSELECT COUNT(*)\n";
+                    text += "\tINTO total_overlap\n";
+                    text += "\tFROM " + this.initialEntity.name;
+                    if (this.initialEntity.getPrimaryKey().length === 1) {
+                        text += "\tWHERE NEW." + this.finalEntity.getPrimaryKey()[0].split(" ")[0] + " = " + this.initialEntity.getPrimaryKey()[0].split(" ")[0] + "\n";
+                    } else if (this.initialEntity.getPrimaryKey().length === 2){
+                        text += "\tWHERE (\n";
+                        text += "\t\t(NEW." + this.finalEntity.getPrimaryKey()[0].split(" ")[0] + " = " + this.initialEntity.getPrimaryKey()[0].split(" ")[0] + " AND NEW." + this.finalEntity.getPrimaryKey()[1].split(" ")[0] + " = " + this.initialEntity.getPrimaryKey()[1].split(" ")[0] + ")\n";
+                        text += "\t\tOR\n";
+                        text += "\t\t(NEW." + this.finalEntity.getPrimaryKey()[0].split(" ")[0] + " = " + this.initialEntity.getPrimaryKey()[1].split(" ")[0] + " AND NEW." + this.finalEntity.getPrimaryKey()[1].split(" ")[0] + " = " + this.initialEntity.getPrimaryKey()[0].split(" ")[0] + ")\n";
+                        text += "\t\t)\n";
+                    }         
+                    text += "\t\tAND (\n";
+                    text += "\t\t\t(NEW." + this.finalEntity.name + "_end > " + this.initialEntity.name + "_start AND NEW." + this.finalEntity.name + "_start <= " + this.initialEntity.name +"_start)\n";
+                    text += "\t\t\tOR\n";
+                    text += "\t\t\t(NEW." + this.finalEntity.name + "_end >= " + this.initialEntity.name + "_end AND NEW." + this.finalEntity.name + "_start < " + this.initialEntity.name +"_end)\n";
+                    text += "\t\t\tOR\n";
+                    text += "\t\t\t(NEW." + this.finalEntity.name + "_start >= " + this.initialEntity.name + "_start AND NEW." + this.finalEntity.name + "_end <= " + this.initialEntity.name +"_end)\n";
+                    text += "\t\t);\n";                      
+                    text += "\tIF total_overlap > 0 THEN\n";
+                    text += "\t\tSIGNAL SQLSTATE '45000'\n";
+                    text += "\t\tSET MESSAGE_TEXT = '" + this.finalEntity.name + " cannot overlap with " + this.initialEntity.name + ".';\n";
+                    text += "\tEND IF;\n";
+                    if (this.optional) {
+                        text += "\tIF total_time < " + this.duration.toString() + " THEN\n";
+                        text += "\t\tSIGNAL SQLSTATE '45000'\n";
+                        text += "\t\tSET MESSAGE_TEXT = '" + this.finalEntity.name + " must have been in " + this.initialEntity.name + " for at least " + this.duration.toString() + " units.';\n";
+                        text += "\tEND IF;\n";
+                        text += "END;\n//\n\n";
+                    }
+                    else {
+                        text += "\tIF total_time <> " + this.duration.toString() + " THEN\n";
+                        text += "\t\tSIGNAL SQLSTATE '45000'\n";
+                        text += "\t\tSET MESSAGE_TEXT = '" + this.finalEntity.name + " must have been in " + this.initialEntity.name + " for " + this.duration.toString() + " units.';\n";
+                        text += "\tEND IF;\n";
+                        text += "END;\n//\n\n";
+                    }
                     if (!this.optional) {
-                        text += "CREATE TRIGGER " + this.initialEntity.name + "_chg_to_" + this.finalEntity.name + "_1\n";
+                        text += "CREATE TRIGGER " + this.initialEntity.name + "_CHG_to_" + this.finalEntity.name + "_2\n";
                         text += "BEFORE INSERT ON " + this.initialEntity.name + "\n";
                         text += "FOR EACH ROW\n";
                         text += "BEGIN\n";
@@ -637,13 +715,18 @@ class Trigger {
                         }
                         text += "\t\tORDER BY " + this.initialEntity.name + "_start;\n";
                         text += "\tDECLARE CONTINUE HANDLER FOR NOT FOUND SET done = 1;\n\n";
+                        text += "\tSET total_time = TIMESTAMPDIFF(" + this.unit + ", NEW." + this.initialEntity.name + "_start, NEW." + this.initialEntity.name + "_end);\n";
+                        text += "\tIF total_time > " + this.duration.toString() + " THEN\n";
+                        text += "\t\tSIGNAL SQLSTATE '45000'\n";
+                        text += "\t\tSET MESSAGE_TEXT = 'No period in " + this.initialEntity.name + " can result in more than " + this.duration.toString() + " continuous units.';\n";
+                        text += "\tEND IF;\n"
                         text += "\tOPEN cursor_a;\n\n";
                         text += "\tread_loop: LOOP\n";
                         text += "\t\tFETCH cursor_a INTO cur_start, cur_end;\n";
                         text += "\t\tIF done THEN\n";
                         text += "\t\t\tLEAVE read_loop;\n";
                         text += "\t\tEND IF;\n\n";
-                        text += "\t\tIF prev_end IS NULL OR PREV_END = cur_start THEN\n";
+                        text += "\t\tIF prev_end IS NULL OR prev_end = cur_start THEN\n";
                         text += "\t\t\tSET total_time = total_time + TIMESTAMPDIFF(" + this.unit + ", cur_start, cur_end);\n";
                         text += "\t\t\tSET prev_end = cur_end;\n"
                         text += "\t\tELSE\n";
@@ -660,7 +743,7 @@ class Trigger {
                     }          
                     // cannot insert date in initial that's past an existing final entry
                     if (this.pinned) {
-                        text += "CREATE TRIGGER " + this.initialEntity.name + "_chg_to_" + this.finalEntity.name + "_2\n";
+                        text += "CREATE TRIGGER " + this.initialEntity.name + "_CHG_to_" + this.finalEntity.name + "_3\n";
                         text += "BEFORE INSERT ON " + this.initialEntity.name + "\n";
                         text += "FOR EACH ROW\n";
                         text += "BEGIN\n";
@@ -684,7 +767,7 @@ class Trigger {
                         text += "\tEND IF;\n";
                         text += "END;\n//";
                     } else { // not pinned, just can't overlap
-                        text += "CREATE TRIGGER " + this.initialEntity.name + "_chg_to_" + this.finalEntity.name + "_2\n";
+                        text += "CREATE TRIGGER " + this.initialEntity.name + "_CHG_to_" + this.finalEntity.name + "_3\n";
                         text += "BEFORE INSERT ON " + this.initialEntity.name + "\n";
                         text += "FOR EACH ROW\n";
                         text += "BEGIN\n";
@@ -756,7 +839,7 @@ class Trigger {
                         text += "\t\tIF done THEN\n";
                         text += "\t\t\tLEAVE read_loop;\n";
                         text += "\t\tEND IF;\n\n";
-                        text += "\t\tIF prev_end IS NULL OR PREV_END = cur_start THEN\n";
+                        text += "\t\tIF prev_end IS NULL OR prev_end = cur_start THEN\n";
                         text += "\t\t\tSET total_time = total_time + TIMESTAMPDIFF(" + this.unit + ", cur_start, cur_end);\n";
                         text += "\t\t\tSET prev_end = cur_end;\n"
                         text += "\t\tELSE\n";
@@ -839,7 +922,7 @@ class Trigger {
                         text += "\tEND IF;\n"
                         text += "END;\n//\n\n";
                     } else {
-                        text += "CREATE TRIGGER " + this.initialEntity.name + "_chg_to_" + this.finalEntity.name + "_1\n";
+                        text += "CREATE TRIGGER " + this.initialEntity.name + "_EXT_to_" + this.finalEntity.name + "_1\n";
                         text += "BEFORE INSERT ON " + this.finalEntity.name + "\n";
                         text += "FOR EACH ROW\n";
                         text += "BEGIN\n";
