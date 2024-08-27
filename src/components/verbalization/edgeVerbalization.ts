33d15cfe740d6d28eaed3b997ff37481519bb1fd
richietaylor <richard@taylorfamily.co.za>
2024-08-27 15:48:36 +0200

    Fixed incorrect verbalizations

--- src/components/verbalization/edgeVerbalization.ts
+++ src/components/verbalization/edgeVerbalization.ts
@@ -307,6 +307,77 @@ export const generateEdgeVerbalization = (
   const isEntity = ['temporalEntity', 'atemporalEntity', 'weakEntity'].includes(String(sourceNodeType)) && ['temporalEntity', 'atemporalEntity', 'weakEntity'].includes(String(targetNodeType));
   const isRelationship = ['temporalRelationship', 'atemporalRelationship', 'weakRelationship'].includes(String(sourceNodeType)) && ['temporalRelationship', 'atemporalRelationship', 'weakRelationship'].includes(String(targetNodeType));
 
+  // if (isEntity) {
+  //   if (quantitative === undefined || quantitative === false) {
+  //     if (optional === 'Optional') {
+  //       if (edgeLabel === 'chg') {
+  //         verbalization += `${capitalizeFirstLetter(getIndefiniteArticle(String(sourceNodeLabel)))} ${sourceNodeLabel} may have been ${getIndefiniteArticle(String(targetNodeLabel))} ${targetNodeLabel} before, but is not ${getIndefiniteArticle(String(targetNodeLabel))} ${targetNodeLabel} now`;
+  //       } else if (edgeLabel === 'CHG') {
+  //         verbalization += `${capitalizeFirstLetter(getIndefiniteArticle(String(sourceNodeLabel)))} ${sourceNodeLabel} may evolve to become ${getIndefiniteArticle(String(targetNodeLabel))} ${targetNodeLabel} ceasing to be ${getIndefiniteArticle(String(sourceNodeLabel))} ${sourceNodeLabel}`;
+  //       } else if (edgeLabel === 'ext') {
+  //         verbalization += `${capitalizeFirstLetter(getIndefiniteArticle(String(sourceNodeLabel)))} ${sourceNodeLabel} may have been ${getIndefiniteArticle(String(targetNodeLabel))} ${targetNodeLabel} before`;
+  //       } else if (edgeLabel === 'EXT') {
+  //         verbalization += `${capitalizeFirstLetter(getIndefiniteArticle(String(sourceNodeLabel)))} ${sourceNodeLabel} may also become ${getIndefiniteArticle(String(targetNodeLabel))} ${targetNodeLabel}`;
+  //       }
+  //     } else if (optional === 'Mandatory') {
+  //       if (edgeLabel === 'chg') {
+  //         verbalization += `Each ${sourceNodeLabel} was ${getIndefiniteArticle(String(targetNodeLabel))} ${targetNodeLabel} before, but is not ${getIndefiniteArticle(String(targetNodeLabel))} ${targetNodeLabel} now`;
+  //       } else if (edgeLabel === 'CHG') {
+  //         verbalization += `Each ${sourceNodeLabel} must evolve to ${targetNodeLabel} ceasing to be ${getIndefiniteArticle(String(sourceNodeLabel))} ${sourceNodeLabel}`;
+  //       } else if (edgeLabel === 'ext') {
+  //         verbalization += `Each ${sourceNodeLabel} was already ${getIndefiniteArticle(String(targetNodeLabel))} ${targetNodeLabel}`;
+  //       } else if (edgeLabel === 'EXT') {
+  //         verbalization += `Each ${sourceNodeLabel} also has to become ${getIndefiniteArticle(String(targetNodeLabel))} ${targetNodeLabel}`;
+  //       }
+  //     }
+  //   } else if (quantitative === true) {
+  //     if (optional === 'Optional') {
+  //       if (edgeLabel === 'chg') {
+  //         verbalization += `${capitalizeFirstLetter(getIndefiniteArticle(String(sourceNodeLabel)))} ${sourceNodeLabel} may have been ${getIndefiniteArticle(String(targetNodeLabel))} ${targetNodeLabel} before for a period of ${maybe}${value} ${timeQuanta}s, but is not ${getIndefiniteArticle(String(targetNodeLabel))} ${targetNodeLabel} now`;
+  //       } else if (edgeLabel === 'CHG') {
+  //         verbalization += `${capitalizeFirstLetter(getIndefiniteArticle(String(sourceNodeLabel)))} ${sourceNodeLabel} may progress to ${getIndefiniteArticle(String(targetNodeLabel))} ${targetNodeLabel} after ${maybe}${value} ${timeQuanta}s, ceasing to be ${getIndefiniteArticle(String(sourceNodeLabel))} ${sourceNodeLabel}`;
+  //       } else if (edgeLabel === 'ext') {
+  //         verbalization += `${capitalizeFirstLetter(getIndefiniteArticle(String(sourceNodeLabel)))} ${sourceNodeLabel} may already be ${getIndefiniteArticle(String(targetNodeLabel))} ${targetNodeLabel} for ${maybe}${value} ${timeQuanta}s since ${value} ${timeQuanta}s`;
+  //       } else if (edgeLabel === 'EXT') {
+  //         verbalization += `${capitalizeFirstLetter(getIndefiniteArticle(String(sourceNodeLabel)))} ${sourceNodeLabel} may also become ${getIndefiniteArticle(String(targetNodeLabel))} ${targetNodeLabel} after ${maybe}${value} ${timeQuanta}s`;
+  //       }
+  //     } else if (optional === 'Mandatory') {
+  //       if (edgeLabel === 'chg') {
+  //         verbalization += `Each ${sourceNodeLabel} was ${getIndefiniteArticle(String(targetNodeLabel))} ${targetNodeLabel} before for a period of ${maybe}${value} ${timeQuanta}s, but is not ${getIndefiniteArticle(String(targetNodeLabel))} ${targetNodeLabel} now`;
+  //       } else if (edgeLabel === 'CHG') {
+  //         verbalization += `Each ${sourceNodeLabel} must progress to ${getIndefiniteArticle(String(targetNodeLabel))} ${targetNodeLabel} after ${maybe}${value} ${timeQuanta}s, ceasing to be ${getIndefiniteArticle(String(sourceNodeLabel))} ${sourceNodeLabel}`;
+  //       } else if (edgeLabel === 'ext') {
+  //         verbalization += `Each ${sourceNodeLabel} was already ${getIndefiniteArticle(String(targetNodeLabel))} ${targetNodeLabel} for ${maybe}${value} ${timeQuanta}s since ${value} ${timeQuanta}s`;
+  //       } else if (edgeLabel === 'EXT') {
+  //         verbalization += `Each ${sourceNodeLabel} will also become ${getIndefiniteArticle(String(targetNodeLabel))} ${targetNodeLabel} after ${maybe}${value} ${timeQuanta}s`;
+  //       }
+  //     }
+  //   }
+  // }
+
+  // if (isRelationship) {
+  //   if (optional === 'Optional') {
+  //     if (edgeLabel === 'chg') {
+  //       verbalization += `${sourceNodeLabel} may have been preceded by ${targetNodeLabel} and they are not in that ${targetNodeLabel} relation now`;
+  //     } else if (edgeLabel === 'CHG') {
+  //       verbalization += `${sourceNodeLabel} may be followed by ${targetNodeLabel}, ending ${sourceNodeLabel}`;
+  //     } else if (edgeLabel === 'ext') {
+  //       verbalization += `${sourceNodeLabel} may be preceded by ${targetNodeLabel} some time earlier`;
+  //     } else if (edgeLabel === 'EXT') {
+  //       verbalization += `${sourceNodeLabel} may be followed by ${targetNodeLabel} some time later`;
+  //     }
+  //   } else if (optional === 'Mandatory') {
+  //     if (edgeLabel === 'chg') {
+  //       verbalization += `Each ${sourceNodeLabel} must have been preceded by ${targetNodeLabel}, and terminating that ${targetNodeLabel} relation`;
+  //     } else if (edgeLabel === 'CHG') {
+  //       verbalization += `Each ${sourceNodeLabel} will be followed by ${targetNodeLabel}, terminating the ${sourceNodeLabel} relation`;
+  //     } else if (edgeLabel === 'ext') {
+  //       verbalization += `Each ${sourceNodeLabel} was preceded by ${targetNodeLabel} some time earlier`;
+  //     } else if (edgeLabel === 'EXT') {
+  //       verbalization += `Each ${sourceNodeLabel} will be followed by ${targetNodeLabel} some time later`;
+  //     }
+  //   }
+  // }
   if (isEntity) {
     if (quantitative === undefined || quantitative === false) {
       if (optional === 'Optional') {
@@ -372,12 +443,21 @@ export const generateEdgeVerbalization = (
       } else if (edgeLabel === 'CHG') {
         verbalization += `Each ${sourceNodeLabel} will be followed by ${targetNodeLabel}, terminating the ${sourceNodeLabel} relation`;
       } else if (edgeLabel === 'ext') {
-        verbalization += `Each ${sourceNodeLabel} was preceded by ${targetNodeLabel} some time earlier`;
+        verbalization += `Each ${sourceNodeLabel} must have been preceded by ${targetNodeLabel} some time earlier`;
       } else if (edgeLabel === 'EXT') {
-        verbalization += `Each ${sourceNodeLabel} will be followed by ${targetNodeLabel} some time later`;
+        verbalization += `Each ${sourceNodeLabel} must be followed by ${targetNodeLabel} some time later`;
       }
     }
   }
+
+    if(persistent)
+  {
+    verbalization += `, and this remains so indefinitely.`
+  }
+  else{
+    verbalization += '.'
+  }
+//else fullstop - remove all other fullstops
 
   return verbalization;
 };
